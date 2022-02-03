0ad9e8465c546edf45e64081d7f23fc8fa56f5d2
the1812 <the1812@outlook.com>
2022-02-03 13:46:26 +0800

    Update data

--- 2022/data.ts
+++ 2022/data.ts
@@ -1,5 +1,229 @@
 import { ListItem } from '../common/update'
 
 export const items: ListItem[] = [
-
+  {
+    time: '00:00',
+    page: 1,
+    title: '关于华灯宴的传说·其一',
+  },
+  {
+    time: '01:03',
+    page: 1,
+    title: '开幕「宴平乐」',
+  },
+  {
+    time: '05:29',
+    page: 1,
+    title: '主持 1 - 灵梦, 文',
+  },
+  {
+    time: '07:41',
+    page: 1,
+    title: '神之乐章 - 幻想乡世界之旅',
+  },
+  {
+    time: '10:26',
+    page: 1,
+    title: '熙攘市场何处在 ~ Immemorial Otomads',
+  },
+  {
+    time: '18:28',
+    page: 1,
+    title: '秘封二人的秘密',
+  },
+  {
+    time: '23:36',
+    page: 1,
+    title: '爱丽丝梦游仙境',
+  },
+  {
+    time: '32:52',
+    page: 1,
+    title: '东喜发财',
+  },
+  {
+    time: '35:54',
+    page: 1,
+    title: '主持 2 - 阿求, 妖梦',
+  },
+  {
+    time: '37:03',
+    page: 1,
+    title: '你是袿姬',
+  },
+  {
+    time: '49:52',
+    page: 1,
+    title: '为你心里小鹿乱撞',
+  },
+  {
+    time: '54:31',
+    page: 1,
+    title: '东方偶像是传奇 重新开始',
+  },
+  {
+    time: '56:57',
+    page: 1,
+    title: '彼与此之乡',
+  },
+  {
+    time: '60:47',
+    page: 1,
+    title: '无法逾越的一寸之壁',
+  },
+  {
+    time: '70:08',
+    page: 1,
+    title: '苍月下的决意',
+  },
+  {
+    time: '75:35',
+    page: 1,
+    title: '嘉宾祝福 1',
+  },
+  {
+    time: '87:02',
+    page: 1,
+    title: 'Welcome Home',
+  },
+  {
+    time: '92:09',
+    page: 1,
+    title: '完全凭依演奏会',
+  },
+  {
+    time: '98:07',
+    page: 1,
+    title: '第 16 回东方 M1 漫才大奖赛 - 巫女巫女 Spark',
+  },
+  {
+    time: '106:14',
+    page: 1,
+    title: '再次来到紫樱树下',
+  },
+  {
+    time: '109:35',
+    page: 1,
+    title: '月的另一侧',
+  },
+  {
+    time: '112:44',
+    page: 1,
+    title: '毛玉大战',
+  },
+  {
+    time: '114:26',
+    page: 1,
+    title: '继续写下去, 直到最后',
+  },
+  {
+    time: '118:07',
+    page: 1,
+    title: '东方热舞派对',
+  },
+  {
+    time: '123:57',
+    page: 1,
+    title: '尘世不变的悲观主义 ~ 心绮楼囃子',
+  },
+  {
+    time: '00:00',
+    page: 2,
+    title: '主持 3 - 灵梦, 文',
+  },
+  {
+    time: '01:24',
+    page: 2,
+    title: 'Midnight glow-resurrection',
+  },
+  {
+    time: '06:14',
+    page: 2,
+    title: 'たぬすがぬ',
+  },
+  {
+    time: '08:55',
+    page: 2,
+    title: '堇梦',
+  },
+  {
+    time: '15:20',
+    page: 2,
+    title: '遇见',
+  },
+  {
+    time: '19:16',
+    page: 2,
+    title: '卖烟花',
+  },
+  {
+    time: '29:38',
+    page: 2,
+    title: '梦闲月谈',
+  },
+  {
+    time: '33:56',
+    page: 2,
+    title: '东方绀珠传 LastBoss 组曲',
+  },
+  {
+    time: '46:02',
+    page: 2,
+    title: '名侦探爱丽丝酱',
+  },
+  {
+    time: '50:58',
+    page: 2,
+    title: '幻想乡の夏踊り',
+  },
+  {
+    time: '56:08',
+    page: 2,
+    title: '天 ~ Ancients',
+  },
+  {
+    time: '61:04',
+    page: 2,
+    title: '最善的选择',
+  },
+  {
+    time: '71:21',
+    page: 2,
+    title: '同人游戏宣传',
+  },
+  {
+    time: '77:42',
+    page: 2,
+    title: '东方dots 战端再起',
+  },
+  {
+    time: '84:42',
+    page: 2,
+    title: '源',
+  },
+  {
+    time: '89:17',
+    page: 2,
+    title: 'Spelling Love',
+  },
+  {
+    time: '93:24',
+    page: 2,
+    title: '神古战场',
+  },
+  {
+    time: '97:09',
+    page: 2,
+    title: '馋死人',
+  },
+  {
+    time: '100:14',
+    page: 2,
+    title: '主持 4 - 阿求, 灵梦, 文, 妖梦',
+  },
+  {
+    time: '102:07',
+    page: 2,
+    title: '谢幕「雁别平沙」',
+  },
 ]
--- 2022/update.ts
+++ 2022/update.ts
@@ -21,12 +21,12 @@ const mainInfo: MainInfo = {
 export const update = () => updateList({
   mainInfo,
   items,
-  singleCount: 21 + 18,
+  singleCount: 21 + 17,
   template: `
 # 【2022·东方华灯宴】节目表
 [【正片】2022·东方华灯宴](https://www.bilibili.com/video/BV16q4y1h7xX)
 
-- 节目标题点击可直接跳转到华灯宴对应分P和时间点
+- 节目标题点击可直接跳转到华灯宴对应分 P 和时间点
 - 时间一栏包含过场动画, 位于每个单品的开头
 - 单品的 av / BV 链接点击可跳转到单品播放页面
 
